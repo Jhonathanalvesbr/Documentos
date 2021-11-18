a5569cb6407897b9e46170d2de50094dc535b1c4
Jhonathanalvesbr <Jhonathanalves_br@hotmail.com>
2021-11-18 11:43:06 -0300

    Update YouTubeDio.js

--- YouTubeDio.js
+++ YouTubeDio.js
@@ -1,1 +1,7 @@
-
+for(var i = 0; i < document.getElementsByTagName("iframe").length; i++){
+	if(document.getElementsByTagName("iframe")[i].src.toString().search("youtube") > -1){
+		var youTube = document.getElementsByTagName("iframe")[0].src.toString().split("?")[0].split("embed");
+		youTube = youTube[0] + "watch?v=" + youTube[1].split("/")[1];
+		console.log(youTube);
+	}
+}
